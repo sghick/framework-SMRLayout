cd0f1a5dae014e5664867bf69e72a50c1a2628b6
dingzhiwen <dingzw@ibaodashi.com>
2021-01-02 16:48:57 +0800

    box

--- SMRLayoutsDemo/examples/widgets/box/SMRBoxDemoController.m
+++ SMRLayoutsDemo/examples/widgets/box/SMRBoxDemoController.m
@@ -15,7 +15,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    UILabel *label1 = [self createLabelWithTitle:@"label1" color:[UIColor blueColor]];
+    UILabel *label2 = [self createLabelWithTitle:@"label2" color:[UIColor yellowColor]];
+    UILabel *label3 = [self createLabelWithTitle:@"label3" color:[UIColor brownColor]];
+    UILabel *label4 = [self createLabelWithTitle:@"label4" color:[UIColor purpleColor]];
     
+    [self.view addSubview:label1];
+    [self.view addSubview:label2];
+    [self.view addSubview:label3];
+    [self.view addSubview:label4];
+    
+    SMRLayout *layout = Box(^(SMRBox * _Nonnull set) {
+        set.view = self.view;
+        set.child = Box(^(SMRBox * _Nonnull set) {
+            set.view = label1;
+            set.child = Box(^(SMRBox * _Nonnull set) {
+                set.view = label2;
+                set.child = Box(^(SMRBox * _Nonnull set) {
+                    set.view = label3;
+                    set.child = Box(^(SMRBox * _Nonnull set) {
+                        set.view = label4;
+                    });
+                });
+            });
+        });
+    });
+    [layout setState];
+}
+
+- (UILabel *)createLabelWithTitle:(NSString *)title color:(UIColor *)color {
+    UILabel *label = [[UILabel alloc] init];
+    label.text = title;
+    label.backgroundColor = color;
+    label.textColor = [UIColor blackColor];
+    return label;
 }
 
 @end
